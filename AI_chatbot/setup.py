cca90f956a9f14a0ea0fa78cd60a5dfd4f85942a
piyuusshh <piiussh@gmail.com>
2026-03-06 13:12:04 +0530

    project setup

---
+++ AI_chatbot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name="SoulTalk",
+    version="0.1.0",
+    author="Piyush Kohli",
+    author_email="piiussh@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
