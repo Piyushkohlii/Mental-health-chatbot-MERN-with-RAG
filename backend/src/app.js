8de123a0944f754940850e362be87b82c0b59b8a
piyuusshh <piiussh@gmail.com>
2026-03-06 19:56:07 +0530

    routes importation and declaration

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,11 +19,15 @@ app.use(cookieParser())
 
 
 //routes import
-// import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
+import chatRoutes from "./routes/chat.routes.js"
 
 //routes declaration
-// app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/chat",chatRoutes)
 
-// http://localhost:8000/api/v1/users/register
+
+
+// http://localhost:port/api/v1/users/register
 
 export {app}
