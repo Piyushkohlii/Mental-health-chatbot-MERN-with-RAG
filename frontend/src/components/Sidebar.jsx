fe572cb4a4ec6c55f43cd71818f0f41adc9f7771
piyuusshh <piiussh@gmail.com>
2026-03-07 23:57:24 +0530

    side bar component

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { IoCloseCircleSharp } from "react-icons/io5";
+import { ChatData } from '../context/ChatContext.jsx';
+import { MdDelete } from "react-icons/md";
+import { LoadingSpinner } from './Loading.jsx';
+import { UserData } from '../context/UserContext.jsx';
+
+
+const Sidebar = ({isOpen, toggleSidebar}) => {
+
+  const {chats, createLoading , createChat , setSelected , deleteChat} = ChatData()
+  
+  const deleteChatHandler = (id) =>{
+    if(confirm("are you really want to delete this chat")){
+      deleteChat(id)
+    }
+  }
+
+  const {logoutHandler} = UserData()
+
+  const clickEvent = (id)=>{
+    setSelected(id)
+    toggleSidebar()
+  }
+
+  return (
+    <div className={`fixed h-screen inset-0 bg-gray-800 p-4 transition-transform transform md:relative md:translate-x-0 md:w-1/4 md:block ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+       <button className='md:hidden p-2 mb-4 bg-gray-700 rounded text-2xl' onClick={toggleSidebar}>
+        <IoCloseCircleSharp/> 
+       </button>
+       <div className='text-2xl font-semibold mb-6'>ChatBot</div>
+       <div className='mb-4'>
+        <button className='w-full py-2 bg-gray-700 hover:bg-gray-600 rounded'
+        onClick={createChat}>
+           {createLoading ? <LoadingSpinner/> : "New Chat +"}
+        </button>
+       </div>
+       <div>
+        <p className='text-sm text-gray-400 mb-2'>Recent Chats</p>
+        <div className='max-h-[500px] overflow-y-auto mb-20 md:mb-0 thinScrollbar'>
+            {
+              chats && chats.length>0 ? chats.map((chat)=>(
+                <div key={chat._id} className='w-full text-left py-2 px-2 bg-gray-700 hover:bg-gray-600 rounded mt-2 flex justify-between items-center'
+                onClick={()=>clickEvent(chat._id)}>
+                  <span>{chat.latestMessage.slice(0,38)}...</span>
+                  <button className='bg-red-600 text-white text-xl px-3 py-2 rounded-md hover:bg-red-700'
+                  onClick={()=>deleteChatHandler(chat._id)}><MdDelete/></button>
+                </div>
+              )) : (<p> No Chats yet</p>)
+            }
+        </div>
+       </div>
+       <div className='absolute bottom-0 mb-6 w-full'>
+       <button className='bg-red-500 text-white text-xl px-3  py-2 rounded-md hover:bg-red-700'
+       onClick={logoutHandler}>Logout</button>
+       </div>
+    </div>
+  )
+}
+
+export default Sidebar
