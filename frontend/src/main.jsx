98c1c13315cef9feaa7c695fff69240bc12bc598
piyuusshh <piiussh@gmail.com>
2026-03-07 23:53:10 +0530

    context api setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
+import { UserProvider } from './context/UserContext.jsx'
+import { ChatProvider } from './context/ChatContext.jsx'
+
+export const server = "http://localhost:5000"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <StrictMode>
-    <App />
+  <BrowserRouter>
+    <UserProvider>
+      <ChatProvider>
+       <App />
+      </ChatProvider> 
+    </UserProvider>
+  </BrowserRouter>
   </StrictMode>
-  </BrowserRouter>,
+  
 )
