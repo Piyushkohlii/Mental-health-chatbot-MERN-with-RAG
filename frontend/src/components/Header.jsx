34474fc61de0dab89774d518a08ede17503d05ee
piyuusshh <piiussh@gmail.com>
2026-03-07 23:53:42 +0530

    created header for home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Modal from './Modal'
+import Login from './Login'
+import Register from './Register'
+import { UserData } from '../context/UserContext'
+
 
 const Header = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isUser, setIsUser] = useState(true)
+
+  const { activeUser, isActive , logoutUser } = UserData()
+
+  const openSignup = () => {
+    setIsUser(false)
+    setIsModalOpen(true)
+  }
+  const openLogin = () => {
+    setIsUser(true)
+    setIsModalOpen(true)
+  }
+
   return (
-     <header className="absolute top-0 left-0 w-full z-20">
+    <header className="absolute top-0 left-0 w-full z-20">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
 
         {/* Logo */}
@@ -13,21 +33,72 @@ const Header = () => {
 
         {/* Navigation */}
         <nav className="hidden md:flex gap-8 text-white font-medium">
-          <Link to="#" className="hover:text-gray-300 transition" >Home</Link>
+          <Link to="/" className="hover:text-gray-300 transition" >Home</Link>
           <Link to="#" className="hover:text-gray-300 transition">About</Link>
-          <Link to="#"className="hover:text-gray-300 transition">Chat</Link>
+          <Link to="/chat" className="hover:text-gray-300 transition">Chat</Link>
           <Link to="#" className="hover:text-gray-300 transition">Resources</Link>
         </nav>
 
         {/* Buttons */}
         <div className="flex gap-4">
-          <button className="text-white hover:text-gray-300">
-            Login
-          </button>
+          {isActive ? (
+            <div className=" relative group">
 
-          <button className="bg-white text-black px-4 py-2 rounded-lg font-medium hover:bg-gray-200">
-            Sign Up
-          </button>
+              {/* Profile Section */}
+              <div className="flex items-center gap-2 cursor-pointer text-white">
+
+                {/* Avatar */}
+                <div className="w-9 h-9 rounded-full bg-blue-400 text-white flex items-center justify-center font-semibold text-2xl">
+                  {activeUser?.fullName?.charAt(0)}
+                </div>
+
+                {/* Name */}
+                <span className="hidden md:block font-medium">
+                  {activeUser?.fullName}
+                </span>
+
+              </div>
+
+              {/* Dropdown Menu */}
+              <div className="absolute right-0 mt-2 w-40 bg-white text-black rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+
+                <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+                  Profile
+                </button>
+
+                <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+                  Chat History
+                </button>
+
+                <button
+                  className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+                  onClick={logoutUser}
+                >
+                  Logout
+                </button>
+
+              </div>
+
+            </div>
+          ) : (
+            <>
+              <button className="text-white hover:text-gray-300"
+                onClick={() => { setIsModalOpen(true); setIsUser(true) }}>
+                Login
+              </button>
+
+              <button className="bg-white text-black px-4 py-2 rounded-lg font-medium hover:bg-gray-200"
+                onClick={() => { setIsModalOpen(true); setIsUser(false) }}>
+                Sign Up
+              </button>
+
+              <Modal modalOpen={isModalOpen} setModalOpen={setIsModalOpen}>
+                {isUser
+                  ? <Login openSignup={openSignup} closeModal={() => setIsModalOpen(false)} />
+                  : <Register openLogin={openLogin} closeModal={() => setIsModalOpen(false)} />}
+              </Modal>
+            </>
+          )}
         </div>
 
       </div>
