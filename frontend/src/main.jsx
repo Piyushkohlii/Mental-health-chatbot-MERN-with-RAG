247d8453f402647240979b54affdac626b2abd8b
piyuusshh <piiussh@gmail.com>
2026-03-09 21:36:54 +0530

    added moodProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,20 +6,23 @@ import { BrowserRouter } from "react-router-dom"
 import { UserProvider } from './context/UserContext.jsx'
 import { ChatProvider } from './context/ChatContext.jsx'
 import { JournalProvider } from './context/JournalContext.jsx'
+import { MoodProvider } from "./context/MoodContext";
 
 export const server = "http://localhost:5000"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
-    <UserProvider>
-      <ChatProvider>
-        <JournalProvider>
-          <App />
-        </JournalProvider>
-      </ChatProvider> 
-    </UserProvider>
-  </BrowserRouter>
+    <BrowserRouter>
+      <UserProvider>
+        <ChatProvider>
+          <MoodProvider>
+            <JournalProvider>
+              <App />
+            </JournalProvider>
+          </MoodProvider>
+        </ChatProvider>
+      </UserProvider>
+    </BrowserRouter>
   </StrictMode>
-  
+
 )
