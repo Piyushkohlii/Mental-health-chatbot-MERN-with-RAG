c6d1ffde7f53f1f0a8f00ca762935af783c8e224
piyuusshh <piiussh@gmail.com>
2026-03-09 16:02:58 +0530

    designed chatheader

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { ChatData } from '../context/ChatContext'
+import { FaRobot } from "react-icons/fa";
 
 const Header = () => {
-    const {chats}=ChatData()
+  const { chats } = ChatData()
   return (
-    <div>
-      <p className='text-lg mb-6'>Hello , How can i help you today</p>
-      {chats && chats.length === 0 && <p className='text-lg mb-6'>Create new chat to continue</p>}
+    <div className='h-18 flex bg-white items-center shadow-lg'>
+      <div className='text-lg  pl-5 flex items-center gap-2'>
+        <span className='bg-green-100 p-2 h-12 w-12 flex items-center justify-center rounded-full text-green-600 text-2xl'><FaRobot /></span>
+        <div>
+          <p className='text-xl'>sOul Talk</p>
+          {chats && chats.length !== 0 && <p className='text-sm'>{"(Active now)"}</p>}
+        </div>
+      </div>
     </div>
   )
 }
