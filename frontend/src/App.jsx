b1027666741f5331dd350b1a9b54b0db7da8f400
piyuusshh <piiussh@gmail.com>
2026-03-09 15:59:09 +0530

    added routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
 import Resources from './pages/Resources'
 import Chat from './pages/chat'
+import Journal from './pages/Journal'
+import AuthChat from './components/AuthChat'
+import CreateJournal from "./pages/CreateJournal"
+import SingleJournal from "./pages/SingleJournal";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/chat" element={<Chat/>} />
+      <Route path="/chat" element={<AuthChat><Chat/></AuthChat>} />
+      <Route path="/journal" element={<AuthChat><Journal/></AuthChat>} />
+      <Route path="/createJournal" element={<CreateJournal/>}/>
+      <Route path="/journal/:id" element={<SingleJournal />} />
       <Route path="/resources" element={<Resources />} />
     </Routes>
   )
